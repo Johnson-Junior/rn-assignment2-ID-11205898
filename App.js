4bcd0b8f10af181473ecdf2eefe98034126204f8
Johnson-Junior <jjkreponi@st.ug.edu.gh>
2024-05-26 01:17:47 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is Johnson</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'beige',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '24',
+    fontWeight: 'bold',
+    
+  },
+});
